98778f813823e283e8989d40c767c7b20e86250c
Axe <shane369@gmail.com>
2021-02-17 22:44:51 -0500

    try reload app

--- dev.js
+++ dev.js
@@ -1,11 +1,37 @@
 const { createServer } = require('vite');
 
 async function bootstrap() {
-  const server = await createServer({
+  const viteServer = await createServer({
     server: { middlewareMode: true },
   });
 
-  await server.ssrLoadModule('./src/main.ts');
+  const { createApp } = await viteServer.ssrLoadModule('./src/main.ts');
+  let app = await createApp;
+
+  // app.use(viteServer.middlewares);
+
+  // app.use('*', async (req, res, next) => {
+  //   try {
+  //     const { createApp } = await viteServer.ssrLoadModule('./src/main.ts');
+  //     app = await createApp;
+  //     app.init();
+  //     next();
+  //   } catch (e) {
+  //     viteServer && viteServer.ssrFixStacktrace(e);
+  //     console.log(e.stack);
+  //     res.status(500).end(e.stack);
+  //   }
+  // });
+
+  await app.listen(3000);
+
+  viteServer.watcher.on('change', async () => {
+    // await app.close();
+    const { createApp } = await viteServer.ssrLoadModule('./src/main.ts');
+    app = await createApp;
+    // await app.listen(3000);
+    await app.init();
+  });
 }
 
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+if (process.env.NODE_ENV === 'production') {
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  }
+
+  bootstrap();
 }
-bootstrap();
+
+export const createApp = NestFactory.create(AppModule);
